30f3cb941613ff3cf4193a0f1e01ca642d9d21d8
Krum Pet <krumpet101@googlemail.com>
2010-02-25 08:56:36 +0000

    initial commit from kp.hcoop.net/kp fossil repo

---
+++ atomix.js
@@ -0,0 +1,386 @@
+
+KP_ATOMIX = (function() {
+
+    var CELL_HEIGHT = 39,
+        CELL_WIDTH = 41,
+        OFFSET_X = 20,
+        OFFSET_Y = 0,
+        item_kind = {
+            '1': 'hydrogen',
+            '2': 'carbon',
+            '3': 'oxygen',
+            '4': 'nitrogen',
+            '5': 'sulphur',
+            '6': 'fluorine',
+            '7': 'chlorine',
+            '8': 'bromine',
+            '9': 'phosphorus',
+            'o': 'crystal',
+            'A': 'connector-horizontal',
+            'B': 'connector-slash',
+            'C': 'connector-vertical',
+            'D': 'connector-backslash'
+        },
+        bond_kind = {
+            'a': 'top-single',
+            'b': 'top-right',
+            'c': 'right-single',
+            'd': 'bottom-right',
+            'e': 'bottom-single',
+            'f': 'bottom-left',
+            'g': 'left-single',
+            'h': 'top-left',
+            'A': 'top-double',
+            'B': 'right-double',
+            'C': 'bottom-double',
+            'D': 'left-double',
+            'E': 'top-triple',
+            'F': 'right-triple',
+            'G': 'bottom-triple',
+            'H': 'left-triple'
+        },
+        gItems,
+        gArrows,
+        gCurrent,
+        gArena,
+
+        iLevel,
+        gLevel,
+        gLevels,
+
+        gg,
+        //
+        $ = xGetElementById;
+
+    function foreach(c, f) {
+        for (var i=0; i < c.length; i++) {
+            f(c[i], i, c);
+        }
+    }
+    function format(s){
+        var count = 0, args = arguments;
+        return s.replace(/\f/g, function() {
+           ++count; return (count < args.length) ? args[count] : '';
+        });
+    }
+    function xpos(col) {
+        return OFFSET_X + col * CELL_WIDTH;
+    }
+    function ypos(row) {
+        return OFFSET_Y + row * CELL_HEIGHT;
+    }
+    function copy_grid(grid){
+        return grid.join('\n').split('\n')
+    }
+
+    function cbHistory(cmd){
+        return function(e){ onHistory(cmd); cancel(); }
+    }
+
+    function setup_controls(self) {
+        xAddEventListener($('next-level'), 'click', onNextLevel, false);
+        xAddEventListener($('prev-level'), 'click', onPrevLevel, false);
+        xAddEventListener($('history-reset'), 'click', cbHistory('reset'), false);
+        xAddEventListener($('history-undo'), 'click', cbHistory('undo'), false);
+        xAddEventListener($('history-redo'), 'click', cbHistory('redo'), false);
+
+    }
+
+    function show_arrow(arrow, row , col) {
+        if (gg.grid[row].charAt(col) == '.') {
+            xTop(arrow, ypos(row));
+            xLeft(arrow,xpos(col));
+        } else {
+            xLeft(arrow, -1000);
+        }
+    }
+
+    function show_arrows(){
+
+        var row = gCurrent.row, col = gCurrent.col;
+
+        show_arrow(gArrows[0], row, col-1);
+        show_arrow(gArrows[1], row, col+1);
+        show_arrow(gArrows[2], row-1, col);
+        show_arrow(gArrows[3], row+1, col);
+    }
+
+    function hide_arrows(){
+        foreach(gArrows, function(a) { xLeft(a, -1000); });
+    }
+
+    function create_arrows(){
+        foreach ( 'left right up down'.split(' '), function(dir) {
+            var cell = new_cell(gArena, 'atom arrow-' + dir, 0, -1000 )
+            gArrows.push(cell)
+            xAddEventListener(cell, 'click', function(evt){onArrowClick(dir);}, false);
+        });
+    }
+
+    function get_item_rowcol(row, col) {
+        var i, item;
+        for (i = 0; i<gItems.length; i++) {
+            item = gItems[i];
+            if (item.row === row && item.col === col) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    function set_current(oAtom) {
+        gCurrent = oAtom;
+        show_arrows(oAtom);
+    }
+
+    function new_cell(parent, cls, top, left) {
+        var element = document.createElement('div');
+        if (parent) {
+            parent.appendChild(element);
+        }
+        xAddClass(element, cls);
+        xTop(element, top);
+        xLeft(element, left);
+        return element;
+    }
+
+    function cancel(e) {
+        xStopPropagation(e);
+        xPreventDefault(e);
+    }
+
+    function decode_move(m){
+        m = m.split('-');
+        m[0] = parseInt(m[0]);
+        m[1] = parseInt(m[1]);
+        m[2] = parseInt(m[2]);
+        m[3] = parseInt(m[3]);
+        return m;
+    }
+
+    function onHistory(cmd) {
+
+        var m, item;
+
+        switch (cmd) {
+
+          case 'reset':
+            start_level(iLevel, true);
+            return
+
+          case 'undo' :
+            if (!gg.history.length) return;
+            m = gg.history.pop();
+            gg.redo.push(m);
+            m = decode_move(m);
+            gCurrent = get_item_rowcol(m[2], m[3]);
+            move_current_atom(m[0], m[1]);
+            return;
+
+          case 'redo' :
+            if (!gg.redo.length) return;
+            m = gg.redo.pop();
+            gg.history.push(m);
+            m = decode_move(m);
+            gCurrent = get_item_rowcol(m[0], m[1]);
+            move_current_atom(m[2], m[3]);
+            return;
+
+          default:
+            return;
+        }
+
+        return
+    }
+
+    function onPrevLevel(e) {
+        if (iLevel > 0) {
+            start_level(iLevel-1);
+        }
+        cancel();
+    }
+
+    function onNextLevel(e) {
+        var l = gLevels.length -1
+        if (iLevel < l) {
+            start_level(iLevel+1);
+        }
+        cancel();
+    }
+
+    function onClick(oAtom) {
+        set_current(oAtom);
+    }
+
+    function onArrowClick(dir) {
+        var row = gCurrent.row,
+            col = gCurrent.col,
+            cr = row,
+            cc = col,
+            grid = gg.grid
+            data = grid[row];
+
+        switch (dir) {
+
+        case 'left':
+            while (data.charAt(col-1) === '.' ) { --col; }
+            break;
+        case 'right':
+            while (data.charAt(col+1) === '.' ) { ++col; }
+            break;
+        case 'up':
+            while (grid[row-1].charAt(col) == '.') { --row; }
+            break;
+        case 'down':
+            while (grid[row+1].charAt(col) == '.') { ++row; }
+            break;
+
+        default:
+            break;
+        }
+
+        if (row !== gCurrent.row || col !== gCurrent.col){
+            gg.history.push(format('\f-\f-\f-\f', cr, cc, row, col));
+            gg.redo = [];
+            move_current_atom(row, col);
+        }
+    }
+
+    function move_current_atom(row, col) {
+
+        var grid = gg.grid,
+            cc = gCurrent.col,
+            cr = gCurrent.row,
+            //
+            data;
+
+        hide_arrows();
+        show_level_data();
+
+        data = grid[cr].charAt(cc);
+        grid[cr] = grid[cr].slice(0, cc) + '.' + grid[cr].slice(cc+1);
+        grid[row] = grid[row].slice(0, col) + data + grid[row].slice(col+1);
+        gCurrent.row = row;
+        gCurrent.col = col;
+        data = 100 * Math.abs(cr - row + cc - col)
+        xAniLine( gCurrent.atom, xpos(col), ypos(row), data, 1, show_arrows);
+    }
+
+
+    function atom_factory(parent, a, row, col, event) {
+
+        var spec = gLevel.atoms[a],
+            atom, oAtom, bonds, bond, kind, item;
+
+        atom = new_cell(parent, 'atom', ypos(row), xpos(col))
+        oAtom = {'row':row, 'col':col, 'atom':atom}
+        if (parent === gArena) gItems.push(oAtom);
+        if (event)
+            xAddEventListener(atom, 'click', function(e) { onClick(oAtom); }, false);
+
+        item = spec[0];
+
+        new_cell(atom, item_kind[spec[0]] + " atom");
+        bonds = spec[1];
+        for (bond=0; bond<bonds.length; bond++) {
+            new_cell(atom, 'bond ' + bond_kind[bonds.charAt(bond)], 0, 0)
+        }
+        return atom;
+    }
+
+    function draw_arena(){
+
+        var item, wall, mol,
+            top, left,
+            row, col;
+
+
+        for (row=0 ; row<gg.grid.length; row++) {
+            item = gg.grid[row];
+            for (col=0; col<item.length; col++) {
+                switch (item.charAt(col)) {
+                    case '#':
+                        new_cell(gArena, 'arena-wall', ypos(row), xpos(col) );
+                        break;
+                    case '.':
+                        break;
+                    default:
+                        atom_factory(gArena, item.charAt(col), row, col, true);
+                }
+            }
+        }
+
+        xHeight(gArena, ypos(row) )//+ OFFSET_Y);
+        xWidth(gArena, xpos(col) + OFFSET_X);
+        xWidth($('controls'),  xpos(col) + OFFSET_X);
+
+        mol = gLevel['molecule'];
+
+        for (row=0 ; row<mol.length; row++) {
+            item = mol[row];
+            for (col=0; col<item.length; col++) {
+                if (item.charAt(col) !== '.') {
+                    atom_factory(gMolecule, item.charAt(col), row, col, false);
+                }
+            }
+        }
+
+        xMoveTo(gMolecule, xLeft(gArena) + xWidth(gArena) + 40, xTop(gArena) + 40 );
+
+        set_current(gItems[0]);
+        create_arrows();
+        show_arrows();
+
+    }
+
+    function show_level_data() {
+         $('leveldata').innerHTML = format('<h4>Level \f: \f (\f moves)</h4>', iLevel+1, gLevel.name, gg.history.length);
+    }
+
+    function reset_level(lvl) {
+        gLevel.gameData = gg = {};
+        gg.grid = copy_grid(gLevel['arena']);
+        gg.history = [];
+        gg.redo = [];
+    }
+
+    function start_level(lvl, reset) {
+
+        lvl = lvl || 0;
+        gItems = [];
+        gArrows = [];
+
+        gArena = $('arena');
+        gArena.innerHTML = '&nbsp;';
+
+        gMolecule = $('molecule')
+        gMolecule.innerHTML =  '&nbsp;';
+
+        gLevel = gLevels[lvl];
+        iLevel = lvl;
+
+        if (!xDef(gLevel.gameData) || reset===true){
+            reset_level()
+        }
+        else {
+            gg = gLevel.gameData;
+        }
+
+        gCurrent = null;
+
+        draw_arena();
+        show_level_data();
+
+    }
+
+    function init(lvl){
+        gLevels = KP_ATOMIX.levels['katomic']
+        setup_controls();
+        start_level(lvl);
+    }
+
+    return {
+        init: init,
+        levels: {}
+    };
+})();
---
+++ atomix_xlib.js
@@ -0,0 +1,313 @@
+/*! Built from X 4.21 by XAG 1.0. 22Feb10 12:28 UT */
+xLibrary={version:'4.21',license:'GNU LGPL',url:'http://cross-browser.com/'};
+// xAddClass r3, Copyright 2005-2007 Daniel Frechette - modified by Mike Foster
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xAddClass(e, c)
+{
+  if ((e=xGetElementById(e))!=null) {
+    var s = '';
+    if (e.className.length && e.className.charAt(e.className.length - 1) != ' ') {
+      s = ' ';
+    }
+    if (!xHasClass(e, c)) {
+      e.className += s + c;
+      return true;
+    }
+  }
+  return false;
+}
+// xAddEventListener r8, Copyright 2001-2007 Michael Foster (Cross-Browser.com)
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xAddEventListener(e,eT,eL,cap)
+{
+  if(!(e=xGetElementById(e)))return;
+  eT=eT.toLowerCase();
+  if(e.addEventListener)e.addEventListener(eT,eL,cap||false);
+  else if(e.attachEvent)e.attachEvent('on'+eT,eL);
+  else {
+    var o=e['on'+eT];
+    e['on'+eT]=typeof o=='function' ? function(v){o(v);eL(v);} : eL;
+  }
+}
+// xAniLine r1, Copyright 2006-2007 Michael Foster (Cross-Browser.com)
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xAniLine(e, x, y, t, a, oe)
+{
+  if (!(e=xGetElementById(e))) return;
+  var x0 = xLeft(e), y0 = xTop(e); // start positions
+  x = Math.round(x); y = Math.round(y);
+  var dx = x - x0, dy = y - y0; // displacements
+  var fq = 1 / t; // frequency
+  if (a) fq *= (Math.PI / 2);
+  var t0 = new Date().getTime(); // start time
+  var tmr = setInterval(
+    function() {
+      var et = new Date().getTime() - t0; // elapsed time
+      if (et < t) {
+        var f = et * fq; // constant velocity
+        if (a == 1) f = Math.sin(f); // sine acceleration
+        else if (a == 2) f = 1 - Math.cos(f); // cosine acceleration
+        f = Math.abs(f);
+        e.style.left = Math.round(f * dx + x0) + 'px'; // instantaneous positions
+        e.style.top = Math.round(f * dy + y0) + 'px';
+      }
+      else {
+        clearInterval(tmr);
+        e.style.left = x + 'px'; // target positions
+        e.style.top = y + 'px';
+        if (typeof oe == 'function') oe(); // 'onEnd' handler
+        else if (typeof oe == 'string') eval(oe);
+      }
+    }, 10 // timer resolution
+  );
+}
+// xCamelize r1, Copyright 2007-2009 Michael Foster (Cross-Browser.com)
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xCamelize(cssPropStr)
+{
+  var i, c, a, s;
+  a = cssPropStr.split('-');
+  s = a[0];
+  for (i=1; i<a.length; ++i) {
+    c = a[i].charAt(0);
+    s += a[i].replace(c, c.toUpperCase());
+  }
+  return s;
+}
+// xClientHeight r6, Copyright 2001-2008 Michael Foster (Cross-Browser.com)
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xClientHeight()
+{
+  var v=0,d=document,w=window;
+  if((!d.compatMode || d.compatMode == 'CSS1Compat') /* && !w.opera */ && d.documentElement && d.documentElement.clientHeight)
+    {v=d.documentElement.clientHeight;}
+  else if(d.body && d.body.clientHeight)
+    {v=d.body.clientHeight;}
+  else if(xDef(w.innerWidth,w.innerHeight,d.width)) {
+    v=w.innerHeight;
+    if(d.width>w.innerWidth) v-=16;
+  }
+  return v;
+}
+// xClientWidth r5, Copyright 2001-2007 Michael Foster (Cross-Browser.com)
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xClientWidth()
+{
+  var v=0,d=document,w=window;
+  if((!d.compatMode || d.compatMode == 'CSS1Compat') && !w.opera && d.documentElement && d.documentElement.clientWidth)
+    {v=d.documentElement.clientWidth;}
+  else if(d.body && d.body.clientWidth)
+    {v=d.body.clientWidth;}
+  else if(xDef(w.innerWidth,w.innerHeight,d.height)) {
+    v=w.innerWidth;
+    if(d.height>w.innerHeight) v-=16;
+  }
+  return v;
+}
+// xDef r1, Copyright 2001-2007 Michael Foster (Cross-Browser.com)
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xDef()
+{
+  for(var i=0; i<arguments.length; ++i){if(typeof(arguments[i])=='undefined') return false;}
+  return true;
+}
+// xGetComputedStyle r7, Copyright 2002-2007 Michael Foster (Cross-Browser.com)
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xGetComputedStyle(e, p, i)
+{
+  if(!(e=xGetElementById(e))) return null;
+  var s, v = 'undefined', dv = document.defaultView;
+  if(dv && dv.getComputedStyle){
+    s = dv.getComputedStyle(e,'');
+    if (s) v = s.getPropertyValue(p);
+  }
+  else if(e.currentStyle) {
+    v = e.currentStyle[xCamelize(p)];
+  }
+  else return null;
+  return i ? (parseInt(v) || 0) : v;
+}
+
+// xGetElementById r2, Copyright 2001-2007 Michael Foster (Cross-Browser.com)
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xGetElementById(e)
+{
+  if (typeof(e) == 'string') {
+    if (document.getElementById) e = document.getElementById(e);
+    else if (document.all) e = document.all[e];
+    else e = null;
+  }
+  return e;
+}
+// xHasClass r3, Copyright 2005-2007 Daniel Frechette - modified by Mike Foster
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xHasClass(e, c)
+{
+  e = xGetElementById(e);
+  if (!e || e.className=='') return false;
+  var re = new RegExp("(^|\\s)"+c+"(\\s|$)");
+  return re.test(e.className);
+}
+// xHeight r7, Copyright 2001-2009 Michael Foster (Cross-Browser.com)
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xHeight(e,h)
+{
+  var css, pt=0, pb=0, bt=0, bb=0, gcs;
+  if(!(e=xGetElementById(e))) return 0;
+  if (xNum(h)) {
+    if (h<0) h = 0;
+    else h=Math.round(h);
+  }
+  else h=-1;
+  css=xDef(e.style);
+  if (e == document || e.tagName.toLowerCase() == 'html' || e.tagName.toLowerCase() == 'body') {
+    h = xClientHeight();
+  }
+  else if(css && xDef(e.offsetHeight) && xStr(e.style.height)) {
+    if(h>=0) {
+      if (document.compatMode=='CSS1Compat') {
+        gcs = xGetComputedStyle;
+        pt=gcs(e,'padding-top',1);
+        if (pt !== null) {
+          pb=gcs(e,'padding-bottom',1);
+          bt=gcs(e,'border-top-width',1);
+          bb=gcs(e,'border-bottom-width',1);
+        }
+        // Should we try this as a last resort?
+        // At this point getComputedStyle and currentStyle do not exist.
+        else if(xDef(e.offsetHeight,e.style.height)){
+          e.style.height=h+'px';
+          pt=e.offsetHeight-h;
+        }
+      }
+      h-=(pt+pb+bt+bb);
+      if(isNaN(h)||h<0) return;
+      else e.style.height=h+'px';
+    }
+    h=e.offsetHeight;
+  }
+  else if(css && xDef(e.style.pixelHeight)) {
+    if(h>=0) e.style.pixelHeight=h;
+    h=e.style.pixelHeight;
+  }
+  return h;
+}
+// xLeft r2, Copyright 2001-2007 Michael Foster (Cross-Browser.com)
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xLeft(e, iX)
+{
+  if(!(e=xGetElementById(e))) return 0;
+  var css=xDef(e.style);
+  if (css && xStr(e.style.left)) {
+    if(xNum(iX)) e.style.left=iX+'px';
+    else {
+      iX=parseInt(e.style.left);
+      if(isNaN(iX)) iX=xGetComputedStyle(e,'left',1);
+      if(isNaN(iX)) iX=0;
+    }
+  }
+  else if(css && xDef(e.style.pixelLeft)) {
+    if(xNum(iX)) e.style.pixelLeft=iX;
+    else iX=e.style.pixelLeft;
+  }
+  return iX;
+}
+// xMoveTo r1, Copyright 2001-2007 Michael Foster (Cross-Browser.com)
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xMoveTo(e,x,y)
+{
+  xLeft(e,x);
+  xTop(e,y);
+}
+// xNum r2, Copyright 2001-2007 Michael Foster (Cross-Browser.com)
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xNum()
+{
+  for(var i=0; i<arguments.length; ++i){if(isNaN(arguments[i]) || typeof(arguments[i])!='number') return false;}
+  return true;
+}
+// xPreventDefault r1, Copyright 2004-2007 Michael Foster (Cross-Browser.com)
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xPreventDefault(e)
+{
+  if (e && e.preventDefault) e.preventDefault();
+  else if (window.event) window.event.returnValue = false;
+}
+// xStopPropagation r1, Copyright 2004-2007 Michael Foster (Cross-Browser.com)
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xStopPropagation(evt)
+{
+  if (evt && evt.stopPropagation) evt.stopPropagation();
+  else if (window.event) window.event.cancelBubble = true;
+}
+// xStr r1, Copyright 2001-2007 Michael Foster (Cross-Browser.com)
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xStr(s)
+{
+  for(var i=0; i<arguments.length; ++i){if(typeof(arguments[i])!='string') return false;}
+  return true;
+}
+// xTop r2, Copyright 2001-2007 Michael Foster (Cross-Browser.com)
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xTop(e, iY)
+{
+  if(!(e=xGetElementById(e))) return 0;
+  var css=xDef(e.style);
+  if(css && xStr(e.style.top)) {
+    if(xNum(iY)) e.style.top=iY+'px';
+    else {
+      iY=parseInt(e.style.top);
+      if(isNaN(iY)) iY=xGetComputedStyle(e,'top',1);
+      if(isNaN(iY)) iY=0;
+    }
+  }
+  else if(css && xDef(e.style.pixelTop)) {
+    if(xNum(iY)) e.style.pixelTop=iY;
+    else iY=e.style.pixelTop;
+  }
+  return iY;
+}
+// xWidth r7, Copyright 2001-2009 Michael Foster (Cross-Browser.com)
+// Part of X, a Cross-Browser Javascript Library, Distributed under the terms of the GNU LGPL
+function xWidth(e,w)
+{
+  var css, pl=0, pr=0, bl=0, br=0, gcs;
+  if(!(e=xGetElementById(e))) return 0;
+  if (xNum(w)) {
+    if (w<0) w = 0;
+    else w=Math.round(w);
+  }
+  else w=-1;
+  css=xDef(e.style);
+  if (e == document || e.tagName.toLowerCase() == 'html' || e.tagName.toLowerCase() == 'body') {
+    w = xClientWidth();
+  }
+  else if(css && xDef(e.offsetWidth) && xStr(e.style.width)) {
+    if(w>=0) {
+      if (document.compatMode=='CSS1Compat') {
+        gcs = xGetComputedStyle;
+        pl=gcs(e,'padding-left',1);
+        if (pl !== null) {
+          pr=gcs(e,'padding-right',1);
+          bl=gcs(e,'border-left-width',1);
+          br=gcs(e,'border-right-width',1);
+        }
+        // Should we try this as a last resort?
+        // At this point getComputedStyle and currentStyle do not exist.
+        else if(xDef(e.offsetWidth,e.style.width)){
+          e.style.width=w+'px';
+          pl=e.offsetWidth-w;
+        }
+      }
+      w-=(pl+pr+bl+br);
+      if(isNaN(w)||w<0) return;
+      else e.style.width=w+'px';
+    }
+    w=e.offsetWidth;
+  }
+  else if(css && xDef(e.style.pixelWidth)) {
+    if(w>=0) e.style.pixelWidth=w;
+    w=e.style.pixelWidth;
+  }
+  return w;
+}
