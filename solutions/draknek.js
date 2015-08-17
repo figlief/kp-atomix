323d7d5b91a0534c3121d4e0ae411aec33c18c88
figlief <figlief@figlief.com>
2015-08-17 17:24:45 +0000

    update solutions

--- solutions/draknek.js
+++ solutions/draknek.js
@@ -2,16 +2,16 @@ if(!KP_ATOMIX){var KP_ATOMIX={}}
 KP_ATOMIX.solutions = [
   {
     "levelSet": "draknek",
-    "lastModified": "2015-08-14 16:29:20",
+    "lastModified": "2015-08-16 13:05:49",
     "source": "http://figlief.pythonanywhere.com/solutions/draknek.json",
     "usage": "Use as you wish but a link to https://code.google.com/p/kp-atomix would be welcome."
   },
   {
     "level": "1",
-    "user": "bob",
-    "moves": 24,
-    "history": "dddedebedfdbbfefefeebedededcffbfbfbebededcdddbdcdedfdfbfbfbeeecececfbefefeffdddfffefefeedfddcfce",
-    "date": "2015-08-14 14:09:06"
+    "user": "masa",
+    "moves": 9,
+    "history": "dddeffefdebebfcfdfdbefeebedeeeefdbdd",
+    "date": "2015-08-16 12:38:04"
   },
   {
     "level": "2",
@@ -22,80 +22,80 @@ KP_ATOMIX.solutions = [
   },
   {
     "level": "3",
-    "user": "",
-    "moves": 0,
-    "history": "",
-    "date": ""
+    "user": "masa",
+    "moves": 24,
+    "history": "dddbbbcbdbdfbfcfefecdfefefededddebdbdbdcecefefdfcdceddbddfddceeeeeefefdfddeddfddedefefdfdfdeddcd",
+    "date": "2015-08-16 13:05:49"
   },
   {
     "level": "4",
-    "user": "",
-    "moves": 0,
-    "history": "",
-    "date": ""
+    "user": "akarma",
+    "moves": 22,
+    "history": "dedfdfffbdbfbfefedeefdfecdbdefbfbfbeeecefededcfcfcfefeeeeeefbebfefcfbfbefffbfbbbbbbcbdcd",
+    "date": "2015-08-15 20:55:56"
   },
   {
     "level": "5",
-    "user": "",
-    "moves": 0,
-    "history": "",
-    "date": ""
+    "user": "akarma",
+    "moves": 29,
+    "history": "ebcbcdccdddfdfcfcfcdcbebcccbebdbcbccefcfcfceceeecdbdbfbebededebebdedbbbdedcdbededbdddebecdcfeeefefdfdfdecfcddddbbdbb",
+    "date": "2015-08-15 21:01:53"
   },
   {
     "level": "6",
-    "user": "",
-    "moves": 0,
-    "history": "",
-    "date": ""
+    "user": "akarma",
+    "moves": 18,
+    "history": "bfcfdbcbcfcccbdbdbdedebebebfbfcfbbbfcfcdbfcfcfcedfdbcecfcfdfdbdedfbfbfbb",
+    "date": "2015-08-15 21:04:02"
   },
   {
     "level": "7",
-    "user": "",
-    "moves": 0,
-    "history": "",
-    "date": ""
+    "user": "akarma",
+    "moves": 9,
+    "history": "ddbddeddddcddcdfcddddddbbddddfdedbdc",
+    "date": "2015-08-15 21:04:54"
   },
   {
     "level": "8",
-    "user": "",
-    "moves": 0,
-    "history": "",
-    "date": ""
+    "user": "akarma",
+    "moves": 11,
+    "history": "bdededeeeecedfdcdccccecdfddddbdcdcececeeccec",
+    "date": "2015-08-15 21:06:30"
   },
   {
     "level": "9",
-    "user": "",
-    "moves": 0,
-    "history": "",
-    "date": ""
+    "user": "akarma",
+    "moves": 34,
+    "history": "fbcbcbcfbbfbffdffbffdfefefeebfbbfffbfbcbcffffffbcbebbbdbddbdbdbbdbcbebdbfbebebecbbbfcbbbbbbedbbbbebcbfbdbdddbcbfddbdbfbebbbcbcccbecebddd",
+    "date": "2015-08-15 21:12:23"
   },
   {
     "level": "10",
-    "user": "",
-    "moves": 0,
-    "history": "",
-    "date": ""
+    "user": "akarma",
+    "moves": 28,
+    "history": "cdcfefdfcfcbcbdbdbdcdcbcddcdedddbcbbdddbdbcbcbcccdcfbbbccfcdccdcbccccccbcdccdcdeccdcdeddddcdcdcccbbbbbbcdfdddded",
+    "date": "2015-08-15 21:15:26"
   },
   {
     "level": "11",
-    "user": "",
-    "moves": 0,
-    "history": "",
-    "date": ""
+    "user": "akarma",
+    "moves": 13,
+    "history": "ebededbdbbebebedbddddddedecececbedebebdbcbcecededbdd",
+    "date": "2015-08-15 21:17:03"
   },
   {
     "level": "12",
-    "user": "",
-    "moves": 0,
-    "history": "",
-    "date": ""
+    "user": "masa",
+    "moves": 42,
+    "history": "efcfdddfcfcbdfcfcfcccbebebefefcfcfcdccfcfcfefebebcbdbefecdcffefcfcbcbdfdfdfcfcccbcbebefefefcfcdcccbcdccccccecfefefebebcbcbcdbcfcfcfefefccdfdfdfefededefefcfdfdbdfedededf",
+    "date": "2015-08-16 12:45:45"
   },
   {
     "level": "13",
-    "user": "",
-    "moves": 0,
-    "history": "",
-    "date": ""
+    "user": "akarma",
+    "moves": 31,
+    "history": "dgdfbgbfbfcffgffffefdfdhdedgdgfgdhddddbddcdheffffgfhfhehehefefdfdfdbfffhdhehehefefdffhffffefbcbbbbcbbdfdcbcddbdefdddfcfdfded",
+    "date": "2015-08-15 21:25:42"
   },
   {
     "level": "14",
